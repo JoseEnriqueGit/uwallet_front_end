96462228976d2f9bf557a497ad8c246b780dd156
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-08 23:55:45 -0400

    Add: custom appearance

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,35 @@
+// Styles
+import Styles from "./Sign-up.module.scss";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+	return (
+		<>
+			<SignUp
+				appearance={{
+					elements: {
+						// Header
+						card: Styles.ContainerLoginSingUp,
+						headerTitle: Styles.headerTitle,
+						headerSubtitle: Styles.headerSubtitle,
+
+						// Form
+						formFieldLabel: Styles.formFieldLabel,
+						formFieldSuccessText: Styles.formFieldSuccessText,
+
+						// Divider
+						dividerText: Styles.dividerText,
+						dividerLine: Styles.dividerLine,
+
+						// Buttons
+						socialButtonsBlockButton: Styles.socialButtons,
+						formButtonPrimary: Styles.formButtonPrimary,
+						// Footer
+						footerActionText: Styles.footerActionText,
+						footerActionLink: Styles.footerActionLink,
+					},
+				}}
+			/>
+		</>
+	);
+}
