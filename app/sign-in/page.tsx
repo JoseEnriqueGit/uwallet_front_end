b37ecca8e53812719b5f482deafb20565d5e57a7
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-09 21:36:41 -0400

    Change: Sign in component clerk and syles

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,43 @@
+// Styles
+import Styles from "./Sign-in.module.scss";
+// Components
+import { SignIn } from "@clerk/nextjs";
+
+
+const Login = () => {
+	return (
+		<SignIn
+			appearance={{
+				elements: {
+					// Container
+					card: Styles.cardContainer,
+
+					// Header
+					header: Styles.header,
+					headerTitle: Styles.headerTitle,
+					headerSubtitle: Styles.headerSubtitle,
+
+					// Form
+					formFieldLabel: Styles.formFieldLabel,
+					formFieldSuccessText: Styles.formFieldSuccessText,
+					// Form Text
+					formFieldErrorText: Styles.formFieldErrorText,
+
+					// Divider
+					dividerText: Styles.dividerText,
+					dividerLine: Styles.dividerLine,
+
+					// Buttons
+					socialButtonsBlockButton: Styles.socialButtons,
+					formButtonPrimary: Styles.formButtonPrimary,
+					// Footer
+					footer: Styles.footer,
+					footerActionText: Styles.footerActionText,
+					footerActionLink: Styles.footerActionLink,
+				},
+			}}
+		/>
+	);
+};
+
+export default Login;
