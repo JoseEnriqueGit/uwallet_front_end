b333f4df0d397463a17fc72d22879e4db2b06da3
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-09 20:02:16 -0400

    Add: appearance elements footer and style

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -25,6 +25,7 @@ export default function Page() {
 						socialButtonsBlockButton: Styles.socialButtons,
 						formButtonPrimary: Styles.formButtonPrimary,
 						// Footer
+						footer: Styles.footer,
 						footerActionText: Styles.footerActionText,
 						footerActionLink: Styles.footerActionLink,
 					},
