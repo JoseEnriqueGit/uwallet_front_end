89e0f36c08f2c1a85a936c8f5af45640812422f3
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-20 22:29:10 -0400

    Add: main route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
 // Css
-import styles from "./page.module.css";
+import Styles from "./page.module.scss";
+// Components
+import { SignIn } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<h1>Uwallet</h1>
-		</main>
+		<SignIn
+			redirectUrl="/overview"
+			signUpUrl="/sign-up"
+			path="/sign-in"
+			appearance={{
+				elements: {
+					// Container
+					card: Styles.cardContainer,
+
+					// Header
+					header: Styles.header,
+					headerTitle: Styles.headerTitle,
+					headerSubtitle: Styles.headerSubtitle,
+
+					// Form
+					formFieldLabel: Styles.formFieldLabel,
+					formFieldSuccessText: Styles.formFieldSuccessText,
+					// Form Text
+					formFieldErrorText: Styles.formFieldErrorText,
+
+					// Divider
+					dividerText: Styles.dividerText,
+					dividerLine: Styles.dividerLine,
+
+					// Buttons
+					socialButtonsBlockButton: Styles.socialButtons,
+					formButtonPrimary: Styles.formButtonPrimary,
+					// Footer
+					footer: Styles.footer,
+					footerActionText: Styles.footerActionText,
+					footerActionLink: Styles.footerActionLink,
+				},
+			}}
+		/>
 	);
 }
