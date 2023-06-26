0bcd295cc083c363e37ba1608d85d9c5d5627732
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-25 23:35:41 -0400

    Add: New Icons

---
+++ src/Components/Icons/Plus.tsx
@@ -0,0 +1,28 @@
+import { IconPropsTypes } from "./Types/IconPropsTypes";
+
+const Plus = ({
+	width = 24,
+	height = 24,
+	fill = "none",
+	stroke = "currentColor",
+	strokeWidth = 3,
+	...props
+}: IconPropsTypes) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    fill="none"
+    stroke={stroke}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={strokeWidth}
+    className="icon icon-tabler icon-tabler-plus"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" />
+    <path d="M12 5v14M5 12h14" />
+  </svg>
+);
+export default Plus;
--- src/app/Components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Input } from './Form/Input/Input';
