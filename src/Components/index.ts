9c9b4cff01a498ee2b6711335fd574db2b8214a2
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-28 22:28:11 -0400

    Change: Barrels

---
+++ src/Components/index.ts
@@ -0,0 +1,8 @@
+export { default as Input } from './Form/Input/Input';
+export { default as NumberInput } from './Form/NumberInput/NumberInput';
+export { default as Coins } from './Icons/Coins';
+export { default as Minus } from './Icons/Minus';
+export { default as NewFile } from './Icons/NewFile';
+export { default as Plus } from './Icons/Plus';
+export { default as SquareRoundedPlus } from './Icons/SquareRoundedPlus';
+export * from './Icons/Types/IconPropsTypes';
