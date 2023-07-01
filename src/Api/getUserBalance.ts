5396300ec579c0620de095f6dc41b4cb92666f24
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-30 23:45:50 -0400

    Add: fetchBalance

---
+++ src/Api/getUserBalance.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+export const fetchBalance = async (userId: string) => {
+  const response = await axios.get(`https://apiuwallet.onrender.com/users/balance/${userId}`);
+  return response.data;
+};
+
+export const useBalance = (userId: string) => {
+  return useQuery(["balance", userId], () => fetchBalance(userId));
+};
+
