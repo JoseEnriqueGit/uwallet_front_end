393d5cf1423affe2e7437f4489c7f21fdd2a0c72
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-30 23:49:50 -0400

    Change: Fragment delete

--- src/Components/Form/Input/Input.tsx
+++ src/Components/Form/Input/Input.tsx
@@ -1,36 +1,36 @@
 import Styles from "./Input.module.scss";
 
 interface IInputProps {
+	label: string;
 	type: string;
 	id?: string;
 	autoComplete?: string;
 	disabled?: boolean;
 	focus?: boolean;
 	defaultValue?: string;
+	value?: string;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 	min?: number;
-	label: string;
 }
 
 const Input = (props: IInputProps) => {
 	return (
-		<>
-			<label className={Styles.Label}>
-				{props.label}
-				<input
-					className={Styles.Input}
-					type={props.type}
-					id={props.id}
-					autoComplete={props.autoComplete}
-					disabled={props.disabled}
-					autoFocus={props.focus}
-					defaultValue={props.defaultValue || ""}
-					onChange={props.onChange}
-					min={props.min}
-					required
-				/>
-			</label>
-		</>
+		<label className={Styles.Label}>
+			{props.label}
+			<input
+				className={Styles.Input}
+				type={props.type}
+				id={props.id}
+				autoComplete={props.autoComplete}
+				disabled={props.disabled}
+				autoFocus={props.focus}
+				// defaultValue={props.defaultValue || ""}
+				value={props.value || ""}
+				onChange={props.onChange}
+				min={props.min}
+				required
+			/>
+		</label>
 	);
 };
 
