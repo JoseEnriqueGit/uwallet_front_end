c24690e62c9fce27df854f05c7df51e328089a94
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-04 21:13:28 -0400

    Change: Barrels update

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,10 +1,12 @@
 export { default as BalanceSection } from './BalanceSection/BalanceSection';
 export { default as Input } from './Form/Input/Input';
 export { default as NumberInput } from './Form/NumberInput/NumberInput';
+export { default as ArrowExchange } from './Icons/ArrowExchange';
 export { default as Coins } from './Icons/Coins';
 export { default as Minus } from './Icons/Minus';
 export { default as NewFile } from './Icons/NewFile';
 export { default as Plus } from './Icons/Plus';
+export { default as SettingDolar } from './Icons/SettingDolar';
 export { default as SquareRoundedPlus } from './Icons/SquareRoundedPlus';
 export * from './Icons/Types/IconPropsTypes';
 export { default as UpdateWallet } from './UpdateWallet/UpdateWallet';
