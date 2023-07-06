2c4b0c50a61213ec4e55ab7b6920242e26d202c4
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-06 16:55:24 -0400

    Change: Icon coin barrel delete

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,7 +2,6 @@ export { default as BalanceSection } from './BalanceSection/BalanceSection';
 export { default as Input } from './Form/Input/Input';
 export { default as NumberInput } from './Form/NumberInput/NumberInput';
 export { default as ArrowExchange } from './Icons/ArrowExchange';
-export { default as Coins } from './Icons/Coins';
 export { default as Minus } from './Icons/Minus';
 export { default as NewFile } from './Icons/NewFile';
 export { default as Plus } from './Icons/Plus';
