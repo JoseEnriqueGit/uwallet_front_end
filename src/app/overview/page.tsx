b635308f42cb42337ff8def6bcf26e253b08d4b8
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-31 21:49:13 -0400

    Change: remove unnecessary components.

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -2,15 +2,7 @@
 // Styles
 import Styles from "./Overview.module.scss";
 // Components
-import {
-	NewFile,
-	SquareRoundedPlus,
-	Plus,
-	Minus,
-	NumberInput,
-	BalanceSection,
-	UpdateWallet,
-} from "@/src/Components";
+import { BalanceSection, UpdateWallet } from "@/src/Components";
 
 const page = () => {
 	return (
