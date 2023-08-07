3e2aa72aa6dddf54538aa28fb06da04667e6f9ec
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-07 17:00:52 -0400

    Change: Add URL rewriting.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
-
-module.exports = nextConfig
+/** @type {import('next')NextConfig} */
+module.exports = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/v1/:path*",
+				destination: "http://localhost:3031/v1/:path*",
+			},
+		];
+	},
+};
