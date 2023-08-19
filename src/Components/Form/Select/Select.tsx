c5a9145305914e5858657b471b225f17ec07ee85
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-18 22:34:28 -0400

    feat: Refactor the currency selection component to integrate user authentication and dynamic data fetching.
    Introduced TypeScript interfaces for ICurrency and ISelectBox to ensure type safety.
    Utilized the React.memo optimization for the SelectBox component.
    Enhanced the useEffect to fetch currency data only when a user is authenticated.

--- src/Components/Form/Select/Select.tsx
+++ src/Components/Form/Select/Select.tsx
@@ -1,22 +1,63 @@
-import React, { useState } from "react";
+"use client";
+import { useAuth } from "@clerk/nextjs";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 import Select from "react-select";
-import { options } from "./dataExample";
 import customStyles from "./CustomStyles";
 
-export default () => {
-	const [isClearable, setIsClearable] = useState(true);
-	const [isSearchable, setIsSearchable] = useState(true);
-	const [isDisabled, setIsDisabled] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-	const [isRtl, setIsRtl] = useState(false);
+interface ICurrency {
+	value: string;
+	label: string;
+}
+
+interface ISelectBox {
+	setCurrency: (currency: ICurrency) => void;
+}
+
+const SelectBox: React.FC<ISelectBox> = React.memo(({ setCurrency }) => {
+	const { userId } = useAuth();
+	const [currencies, setCurrencies] = useState<ICurrency[]>([]);
+
+	const fetchCurrencies = async (): Promise<ICurrency[]> => {
+		try {
+			const response = await axios.get<ICurrency[]>(
+				`/v1/users/${userId}/get-currency-rates`
+			);
+			return response.data;
+		} catch (error) {
+			console.error("An error occurred while fetching the data:", error);
+			throw new Error("An error occurred while fetching the data.");
+		}
+	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (userId) {
+				try {
+					const response = await fetchCurrencies();
+					setCurrencies(response);
+				} catch (error) {
+					console.error("An error occurred while fetching the data:", error);
+				}
+			}
+		};
+
+		fetchData();
+	}, [userId]);
+
+	const options = userId
+		? currencies
+		: [{ value: "NO DATA", label: "NO DATA" }];
 
 	return (
 		<Select
 			styles={customStyles}
 			noOptionsMessage={() => "NOT FOUND"}
-			defaultValue={options[0]}
 			options={options}
+			onChange={(selectedOption) => setCurrency(selectedOption as ICurrency)}
 		/>
 	);
-};
+});
+
+export default SelectBox;
