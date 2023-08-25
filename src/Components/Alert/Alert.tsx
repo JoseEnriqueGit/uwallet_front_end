7ff308f4cb5e14773bdeb72f5a28cb9cd8c4760d
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-25 19:34:23 -0400

    New: alert component

---
+++ src/Components/Alert/Alert.tsx
@@ -0,0 +1,49 @@
+import Styles from "./Alert.module.scss";
+
+import React, { useEffect, useState } from "react";
+
+interface IAlertProps {
+	message: string;
+}
+
+enum AlertType {
+	NotAmountInserted = "NotAmountInserted",
+	InsufficientFunds = "InsufficientFunds",
+}
+
+// Render alert messages
+const renderAlert = new Map();
+renderAlert.set(AlertType.NotAmountInserted, "Please enter a value.");
+renderAlert.set(AlertType.InsufficientFunds, "Insufficient funds.");
+
+const Alert = ({ message }: IAlertProps): JSX.Element => {
+	const [timeOut, setTimeOut] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (!timeOut) {
+				setTimeOut(true);
+			}
+		}, 4000);
+
+		return () => clearTimeout(timer);
+	}, [timeOut]);
+
+	const multiConditionalInit = (itemToGet: string) => {
+		return renderAlert.has(itemToGet) ? (
+			renderAlert.get(itemToGet)
+		) : (
+			<div>Something went wrong...</div>
+		);
+	};
+
+	return (
+		<>
+			{timeOut ? null : (
+				<span className={Styles.Warning}>{multiConditionalInit(message)}</span>
+			)}
+		</>
+	);
+};
+
+export default Alert;
